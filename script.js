afe59c7b3451eaad15c5a7edaba39e3b1ff57790
melekzaidi <79268865+melekzaidi@users.noreply.github.com>
2021-07-26 14:57:15 +0200

    Update script.js

--- script.js
+++ script.js
@@ -330,5 +330,298 @@ c=parseFloat(st2)-parseFloat(st1) ;
 a.innerHTML=c.toString()  
 
    
+}
+op=""  ;
+}
+
+    a.innerHTML=c.toString()
+
+}
+else if (op==="*"){
+a = document.getElementById("current") ; 
+st1=a.innerHTML;
+b=document.getElementById("prev") ; 
+st2=b.innerHTML;
+b.innerHTML="" ;
+c=parseFloat(st2)*parseFloat(st1) ; 
+
+a.innerHTML=c.toString()  
+}
+else if(op==="+"){
+    a = document.getElementById("current") ; 
+st1=a.innerHTML;
+b=document.getElementById("prev") ; 
+st2=b.innerHTML;
+b.innerHTML="" ;
+c=parseFloat(st2)+parseFloat(st1) ; 
+
+a.innerHTML=c.toString()  }
+else if(op==="-"){
+    a = document.getElementById("current") ; 
+st1=a.innerHTML;
+b=document.getElementById("prev") ; 
+st2=b.innerHTML;
+b.innerHTML="" ;
+c=parseFloat(st2)-parseFloat(st1) ; 
+
+a.innerHTML=c.toString()  
+
+}
+
+
+     a = document.getElementById("current") ; 
+    st1=a.innerHTML;
+    op="/";
+    a.innerHTML="" ;
+    b=document.getElementById("prev") ; 
+    st2=b.innerHTML;
+    if ((st1=="")|| (st1=="0") || (st1.indexOf("error")!=-1)){
+    b.innerHTML="error" ;}
+    else{
+        b.innerHTML=st1 ;  
+    }
+    if (st2!="")
+    {   
+        c=parseFloat(st2)/parseFloat(st1) ; 
+        
+        b.innerHTML=c.toString()
+      
+        
+    }   }
+ 
+
+function mult(){
+    if (op!=="")if (op==="/"){
+        a = document.getElementById("current") ; 
+        st1=a.innerHTML;
+        b=document.getElementById("prev") ; 
+        st2=b.innerHTML;
+        b.innerHTML="" ;
+        c=parseFloat(st2)/parseFloat(st1) ; 
+    r=c;
+    a.innerHTML=c.toString()
+
+}
+else if (op==="*"){
+a = document.getElementById("current") ; 
+st1=a.innerHTML;
+b=document.getElementById("prev") ; 
+st2=b.innerHTML;
+b.innerHTML="" ;
+c=parseFloat(st2)*parseFloat(st1) ; 
+
+a.innerHTML=c.toString()  
+}
+else if(op==="+"){
+    a = document.getElementById("current") ; 
+st1=a.innerHTML;
+b=document.getElementById("prev") ; 
+st2=b.innerHTML;
+b.innerHTML="" ;
+c=parseFloat(st2)+parseFloat(st1) ; 
+
+a.innerHTML=c.toString()  }
+else if(op==="-"){
+    a = document.getElementById("current") ; 
+st1=a.innerHTML;
+b=document.getElementById("prev") ; 
+st2=b.innerHTML;
+b.innerHTML="" ;
+c=parseFloat(st2)-parseFloat(st1) ; 
+
+a.innerHTML=c.toString()  
+
+}
+    a = document.getElementById("current") ; 
+    st1=a.innerHTML;
+    op="*";
+    a.innerHTML="" ;
+    b=document.getElementById("prev") ; 
+    st2=b.innerHTML;
+    if ((st1=="")|| (st1=="0") || (st1.indexOf("error")!=-1)){
+    b.innerHTML="error" ;}
+    else{
+        b.innerHTML=st1 ;  
+    }
+    if (st2!="")
+    {   
+        c=parseFloat(st2)*parseFloat(st1) ; 
+        
+        b.innerHTML=c.toString()
+      
+        
+    }
+ 
+    
+  
+    }
+ 
+function plus(){
+    if (op!=="")if (op==="/"){
+        a = document.getElementById("current") ; 
+        st1=a.innerHTML;
+        b=document.getElementById("prev") ; 
+        st2=b.innerHTML;
+        b.innerHTML="" ;
+        c=parseFloat(st2)/parseFloat(st1) ; 
+    r=c;
+    a.innerHTML=c.toString()
+
+}
+else if (op==="*"){
+a = document.getElementById("current") ; 
+st1=a.innerHTML;
+b=document.getElementById("prev") ; 
+st2=b.innerHTML;
+b.innerHTML="" ;
+c=parseFloat(st2)*parseFloat(st1) ; 
+
+a.innerHTML=c.toString()  
+}
+else if(op==="+"){
+    a = document.getElementById("current") ; 
+st1=a.innerHTML;
+b=document.getElementById("prev") ; 
+st2=b.innerHTML;
+b.innerHTML="" ;
+c=parseFloat(st2)+parseFloat(st1) ; 
+
+a.innerHTML=c.toString()  }
+else if(op==="-"){
+    a = document.getElementById("current") ; 
+st1=a.innerHTML;
+b=document.getElementById("prev") ; 
+st2=b.innerHTML;
+b.innerHTML="" ;
+c=parseFloat(st2)-parseFloat(st1) ; 
+
+a.innerHTML=c.toString()  
+
+    
+}a = document.getElementById("current") ; 
+st1=a.innerHTML;
+op="+";
+a.innerHTML="" ;
+b=document.getElementById("prev") ; 
+st2=b.innerHTML;
+if ((st1=="")|| (st1=="0") || (st1.indexOf("error")!=-1)){
+b.innerHTML="error" ;}
+else{
+    b.innerHTML=st1 ;  
+}
+if (st2!="")
+{   
+    c=parseFloat(st2)*parseFloat(st1) ; 
+    
+    b.innerHTML=c.toString()
+  
+    
+}
+}
+function sub(){
+    if (op!=="")if (op==="/"){
+        a = document.getElementById("current") ; 
+        st1=a.innerHTML;
+        b=document.getElementById("prev") ; 
+        st2=b.innerHTML;
+        b.innerHTML="" ;
+        c=parseFloat(st2)/parseFloat(st1) ; 
+    r=c;
+    a.innerHTML=c.toString()
+
+}
+else if (op==="*"){
+a = document.getElementById("current") ; 
+st1=a.innerHTML;
+b=document.getElementById("prev") ; 
+st2=b.innerHTML;
+b.innerHTML="" ;
+c=parseFloat(st2)*parseFloat(st1) ; 
+
+a.innerHTML=c.toString()  
+}
+else if(op==="+"){
+    a = document.getElementById("current") ; 
+st1=a.innerHTML;
+b=document.getElementById("prev") ; 
+st2=b.innerHTML;
+b.innerHTML="" ;
+c=parseFloat(st2)+parseFloat(st1) ; 
+
+a.innerHTML=c.toString()  }
+else if(op==="-"){
+    a = document.getElementById("current") ; 
+st1=a.innerHTML;
+b=document.getElementById("prev") ; 
+st2=b.innerHTML;
+b.innerHTML="" ;
+c=parseFloat(st2)-parseFloat(st1) ; 
+
+a.innerHTML=c.toString()  
+
+    
+}a = document.getElementById("current") ; 
+st1=a.innerHTML;
+op="-";
+a.innerHTML="" ;
+b=document.getElementById("prev") ; 
+st2=b.innerHTML;
+if ((st1=="")|| (st1=="0") || (st1.indexOf("error")!=-1)){
+b.innerHTML="error" ;}
+else{
+    b.innerHTML=st1 ;  
+}
+if (st2!="")
+{   
+    c=parseFloat(st2)-parseFloat(st1) ; 
+    
+    b.innerHTML=c.toString()
+  
+    
+}
+}
+
+function egale(){
+    if (op!=="")if (op==="/"){
+        a = document.getElementById("current") ; 
+        st1=a.innerHTML;
+        b=document.getElementById("prev") ; 
+        st2=b.innerHTML;
+        b.innerHTML="" ;
+        c=parseFloat(st2)/parseFloat(st1) ; 
+    r=c;
+    a.innerHTML=c.toString()
+
+}
+else if (op==="*"){
+a = document.getElementById("current") ; 
+st1=a.innerHTML;
+b=document.getElementById("prev") ; 
+st2=b.innerHTML;
+b.innerHTML="" ;
+c=parseFloat(st2)*parseFloat(st1) ; 
+
+a.innerHTML=c.toString()  
+}
+else if(op==="+"){
+    a = document.getElementById("current") ; 
+st1=a.innerHTML;
+b=document.getElementById("prev") ; 
+st2=b.innerHTML;
+b.innerHTML="" ;
+c=parseFloat(st2)+parseFloat(st1) ; 
+
+a.innerHTML=c.toString()  }
+else if(op==="-"){
+    a = document.getElementById("current") ; 
+st1=a.innerHTML;
+b=document.getElementById("prev") ; 
+st2=b.innerHTML;
+b.innerHTML="" ;
+c=parseFloat(st2)-parseFloat(st1) ; 
+
+a.innerHTML=c.toString()  
+
+   
 } op="" ;
 }
